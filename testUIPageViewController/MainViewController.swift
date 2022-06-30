1fbd73e4783e365d82ca706c8fda9b32105be61a
aPuYue <pu.yue@access-company.com>
2022-06-30 19:39:14 +0900

    成功取得pageVC实例，运用delegete改写， 有UI反应慢问题

--- testUIPageViewController/MainViewController.swift
+++ testUIPageViewController/MainViewController.swift
@@ -8,6 +8,10 @@
 import Foundation
 import UIKit
 
+protocol SwipeDelegate {
+    func didSwipe(_ index: Int)
+}
+
 class MainViewController: UIViewController {
     
     @IBOutlet weak var containerView: UIView!
@@ -16,13 +20,28 @@ class MainViewController: UIViewController {
     var pageViewControllerObject: PageViewController?
     
     @IBAction func didClick(_ sender: Any) {
-        //object 或者 userInfo来传值
-        NotificationCenter.default.post(name: Notification.Name("DidClick"), object: switchButton.selectedSegmentIndex)
-//        NotificationCenter.default.post(name: Notification.Name("DidClick"), object: switchButton.selectedSegmentIndex, userInfo: ["index": switchButton.selectedSegmentIndex])
+        guard let pageViewControllerObject = pageViewControllerObject else { return }
+        pageViewControllerObject.didClick(switchButton.selectedSegmentIndex)
     }
         
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // https://stackoverflow.com/questions/29582200/how-do-i-get-the-views-inside-a-container-in-swift
+        //使用这个能得到同一个sb中所有 vc实例
+        //segue.identifier == "EmbedSegue" 这个ID是segue的不是VC的
+        if let vc = segue.destination as? PageViewController {
+            self.pageViewControllerObject = vc
+            vc.delegate02 = self
+        }
+    }
+}
+
+extension MainViewController: SwipeDelegate {
+    // https://www.jianshu.com/p/761068184153
+    func didSwipe(_ index: Int) {
+        switchButton.selectedSegmentIndex = index
     }
 }
--- testUIPageViewController/PageViewController.swift
+++ testUIPageViewController/PageViewController.swift
@@ -10,33 +10,19 @@ import UIKit
 class PageViewController: UIPageViewController {
     
     private var controllers: [UIViewController] = []
+    
+    var delegate02: SwipeDelegate?
+    
+//    var currentPage = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.dataSource = self
+        self.delegate = self
 
         initPageView()
-        setupNotification()
     }
-
-    private func setupNotification() {
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(didClick),//触发什么函数
-            name: NSNotification.Name(rawValue: "DidClick"),//自定义名称
-            object: nil
-        )
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(
-            self,
-            name: NSNotification.Name(rawValue: "DidClick"),
-            object: nil
-        )
-    }
-
     
     func initPageView(){
         // PageViewControllerで表示するViewControllerをインスタンス化
@@ -55,43 +41,81 @@ class PageViewController: UIPageViewController {
         // PageViewControllerのDataSourceとの関連付け
     }
 
-    @objc func didClick(notification: Notification) {
-        if let index = notification.object as? Int {
-            switch index {
-            case 0:
-                self.setViewControllers([self.controllers[0]], direction: .reverse, animated: true, completion: nil)//注意从2回到1是往前翻页
-            default:
-                self.setViewControllers([self.controllers[1]], direction: .forward, animated: true, completion: nil)
-            }
+    @objc func didClick(_ index: Int) {
+        switch index {
+        case 0:
+//            currentPage = 0
+            self.setViewControllers([self.controllers[0]], direction: .reverse, animated: true, completion: nil)//注意从2回到1是往前翻页
+        default:
+//            currentPage = 1
+            self.setViewControllers([self.controllers[1]], direction: .forward, animated: true, completion: nil)
         }
     }
 
 }
 
-extension PageViewController: UIPageViewControllerDataSource {
-    // スクロールするページ数
+extension PageViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    // 全ページ数を返すメソッド
     func presentationCount(for pageViewController: UIPageViewController) -> Int {
         return self.controllers.count
     }
+    
+    //現在のページを返すメソッド
+//    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+//        return currentPage
+//    }
 
-    // 左にスワイプした時の処理
+    // 左にスワイプした時の処理 Next 0》 1
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         if let index = self.controllers.firstIndex(of: viewController),
             index < self.controllers.count - 1 {
-            return self.controllers[index + 1]
+            return self.controllers[index + 1] // 1
         } else {
-            return nil
+            return nil // 这里是为了，已经是最后一个还往下一个
         }
     }
     
-    // 右にスワイプした時の処理
+    // 右にスワイプした時の処理 前一个 1》 0
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         if let index = self.controllers.firstIndex(of: viewController),
             index > 0 {
-            return self.controllers[index - 1]
+            return self.controllers[index - 1] // 0
         } else {
-            return nil
+            return nil // 这里是为了，已经是第一个还往前一个
+        }
+    }
+    
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        didFinishAnimating finished: Bool,
+        previousViewControllers: [UIViewController],
+        transitionCompleted completed: Bool
+    ) {
+
+        guard
+            let newViewController = pageViewController.viewControllers?.first,
+            let newIndex = self.controllers.firstIndex(of: newViewController)
+        else { return }
+        DispatchQueue.main.async { [weak self] in
+            self?.delegate02?.didSwipe(newIndex)
+//            self?.isScrollEnabled = true
+            //手指滑动触发UISegmentedControl改变UI部分有一个问题,UI改变的太慢，手指可以来回动，导致index不对
+            // self?.isScrollEnabled = true 就是为了这个，
         }
     }
 }
 
+//extension UIPageViewController {
+//    var scrollView: UIScrollView? {
+//        return view.subviews.first(where: { $0 is UIScrollView }) as? UIScrollView
+//    }
+//
+//    var isScrollEnabled: Bool {
+//        get {
+//            return scrollView?.isScrollEnabled ?? true
+//        }
+//        set {
+//            scrollView?.isScrollEnabled = newValue
+//        }
+//    }
+//}
